c3c7a89b2255f281dc4ba456b8cc81ed6ad0b1b9
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-12-11 21:57:23 +0100

    Update server.js

--- altv/server.js
+++ altv/server.js
@@ -5,12 +5,12 @@ dotenv.config();
 let TopListUpdate;
 const neededValues = [
     'minage',
-	'whitelist',
+    'whitelist',
     'language',
     'framework',
     'voice',
-	'chat',
-    'secretToken'
+    'chat',
+    'secretPID'
 ];
 neededValues.forEach((value) => {
     if (!(value in process.env)) {
@@ -18,27 +18,44 @@ neededValues.forEach((value) => {
         process.exit(0);
     }
 });
-function Updater(){
-    axios({
-        method: 'post',
-        url: 'https://gta-spot.com/announce.php',
-        data: {
-            minage: process.env.minage,
-            whitelist: process.env.whitelist,
-            language: process.env.language,
-            framework: process.env.framework,
-            voice: process.env.voice,
-            chat: process.env.chat,
-            servername: alt.getServerConfig().name,
-            ip: alt.ServerIp(),
-            port: alt.ServerPort(),
-            gamemode: alt.getServerConfig().gamemode,
-            platform: 'altv',
-            onlineplayers: alt.Player.all.length,
-            maxplayers: alt.getServerConfig().players,
-            secretToken: alt.getServerConfig().secretToken,
-            secretToken2: process.env.secretToken
+async function doPostRequest() {
+    var postdata = {
+        minage: process.env.minage,
+        whitelist: process.env.whitelist,
+        language: process.env.language,
+        framework: process.env.framework,
+        voice: process.env.voice,
+        chat: process.env.chat,
+        servername: alt.getServerConfig().name,
+        ip: alt.ServerIp,
+        port: alt.ServerPort,
+        gamemode: alt.getServerConfig().gamemode,
+        platform: 'altv',
+        onlineplayers: alt.Player.all.length,
+        maxplayers: alt.getServerConfig().players,
+        secretToken: alt.getServerConfig().secretSID,
+        secretToken2: process.env.secretPID
+    };
+    var headers = {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
         }
+    await axios.post('https://gta-spot.com/announce.php', postdata, headers)
+    .then(response => {
+        if (response.data == "ok")
+            alt.log("data transfer success");
+        else
+            alt.log(response.data);
+    })
+      .catch(error => {
+        if (error.response)
+            alt.log(error.response.status);
+        else if (error.request)
+            alt.log(error.request);
+        else
+            alt.log(error.message);
     });
 }
-TopListUpdate = alt.setInterval(Updater, 1800000);
+TopListUpdate = alt.setInterval(doPostRequest, 540000);
+doPostRequest();
